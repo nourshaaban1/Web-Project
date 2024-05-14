72560ffb8122dbab982246d6d745c8af42bc0a8a
Nier0o <ahmedsaberahmed616@gmail.com>
2024-05-14 15:05:03 +0300

    fk nour sha3baan malk al html w css bs

--- Django/Library_Management/Book/views.py
+++ Django/Library_Management/Book/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def all_books(request):
+    all_books = Book.objects.all()
+    return render(request, 'Book\all-book.html', {'books' : Book})
+
